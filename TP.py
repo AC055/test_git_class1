ab5a714784eb6c593401a816ab757d1b42bc7134
AC055 <ac.cora5217@gmail.com>
2026-06-02 14:53:38 +0000

    this is branch test

--- TP.py
+++ TP.py
@@ -1,6 +1,7 @@
 # Step 1: Basic output
 print("Welcome to the Git exercise!")
 print("Ciao")
+print("THis is the branch test")
 
 # Step 2: A simple calculation (buggy)
 # def subtract(a, b):
@@ -10,6 +11,7 @@ print("Ciao")
 # Step 3: Random fun
 import random
 print("Lucky number:", random.randint(1, 100))
+
 
 # Step 4: Final message
 # print("Great job completing the steps!")
